6e11dafc9bc10e36a77ab49546310daeb925a8d0
Duncan Ragsdale <88173870+Thistleman@users.noreply.github.com>
2023-04-17 09:06:25 -0700

    added example test, cleaned up ci-cd workflows

---
+++ tests/test_placeholder.py
@@ -0,0 +1,8 @@
+# Rename the file as desired for your testing structure/type, 
+# or add new files following the format below.
+# E.G. test_<your-test-name>.py
+# This contains an example test that always passes.
+# When ready, you can add proper tests for your algorithm in this folder.
+
+def test_always_passes():
+    assert 1 + 1 == 2
